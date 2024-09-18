b9c365c73c4e5ab5b8a9334b5fdb6e67baf428ba
flash1999 <noursoury99@gmail.com>
2024-09-18 12:53:30 +0300

    initial release

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to root_path
+    redirect_to root_path, notice: 'Signed out successfully.'
   end
 end
