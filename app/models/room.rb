cd985c1f2c9d190a905a09e5f1c7349684ba4d81
flash1999 <noursoury99@gmail.com>
2024-09-18 11:23:04 +0300

    added the turbo rails

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,5 @@ class Room < ApplicationRecord
   validates_uniqueness_of :name
   # this scope to fetch all public rooms for our room list
   scope :public_rooms, -> {where(is_private: false)}
+  after_create_commit {broadcast_append_to "rooms"}
 end
